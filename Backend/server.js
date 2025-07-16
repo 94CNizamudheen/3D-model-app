e860659bec3e4e3df794ca975dd68dc3b4b0dfb8
Nizamudheen <nisunisam2@gmail.com>
2025-07-16 19:08:25 +0530

    limit specified

--- Backend/server.js
+++ Backend/server.js
@@ -11,7 +11,8 @@ app.use(cors({
   origin: process.env.FRONTEND_URL,
 }));
 
-app.use(express.json());
+app.use(express.json({limit:'100mb'}));
+app.use(express.urlencoded({ extended: true, limit: '100mb' }));
 
 connectDB()
   .then(() => {
