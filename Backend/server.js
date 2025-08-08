3bd9bcea16fe7c77c33a4b81b57afdb0a29e28b0
Nizamudheen <nisunisam2@gmail.com>
2025-08-08 15:45:33 +0530

    file size increased

--- Backend/server.js
+++ Backend/server.js
@@ -12,7 +12,7 @@ app.use(cors({
 }));
 
 app.use(express.json({limit:'250mb'}));
-app.use(express.urlencoded({ extended: true, limit: '100mb' }));
+app.use(express.urlencoded({ extended: true, limit: '250mb' }));
 
 connectDB()
   .then(() => {
